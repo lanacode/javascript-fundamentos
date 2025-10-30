e0819889f49f5040d995ef014ba8dc83153ac2c2
Ilana Nascimento - ARTDR <artedasredes@gmail.com>
2025-10-30 10:58:20 -0300

    Create dados.js

---
+++ CatalogoLivros/dados.js
@@ -0,0 +1,49 @@
+const dados = {
+    "description": "tarot cards and basic information about them, as well as references to corresponding scans",
+    "books": [
+        {
+            "imgCapa": "https://s3.novatec.com.br/capas/9788575226834.jpg",
+            "descricao": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi eleifend, purus quis laoreet faucibus, ante augue malesuada mi, id rhoncus augue lorem eget elit. Ut sollicitudin sodales purus.",
+            "titulo": "Primeiros passos com a linguagem Rust",
+            "autor": "José Augusto N. G. Manzano",
+        },
+
+        {
+            "imgCapa": "https://s3.novatec.com.br/capas/9788575226674.jpg",
+            "descricao": "Phasellus libero felis, blandit nec, commodo ut, imperdiet ut, nibh. Suspendisse potenti. Donec ullamcorper cursus dolor. Duis vitae ipsum. Maecenas dapibus hendrerit diam. Morbi varius, massa id pretium accumsan, nunc lorem congue libero, ut euismod metus libero id nulla.",
+            "titulo": "Programação em Baixo Nível",
+            "autor": "Igor Zhirkov",
+        },
+
+        {
+            "imgCapa": "https://s3.novatec.com.br/capas/9788575226681.jpg",
+            "pgLivro": "livros/introducao-a-linguagem-lua",
+            "descricao": "Duis posuere odio sed velit vulputate venenatis. Suspendisse et dui ac metus auctor fringilla. Curabitur interdum augue a pede.>",
+            "titulo": "Introdução à linguagem Lua",
+            "autor": "José Augusto N. G. Manzano",
+        },
+
+        {
+            "imgCapa": "https://s3.novatec.com.br/capas/9788575226568.jpg",
+            "descricao": "Duis cursus, dui non dictum tincidunt, wisi ipsum mollis wisi, nec ornare velit ipsum eget enim. In sed felis. Phasellus condimentum sodales nulla. Etiam orci leo, rutrum malesuada, congue vel, fringilla vitae, lorem. Pellentesque ligula.",
+            "titulo": "Lógica de Programação com JavaScript",
+            "autor": "Edécio Fernando Iepsen",
+        },
+
+        {
+            "imgCapa": "https://s3.novatec.com.br/capas/9788575225905.jpg",
+            "pgLivro": "livros/php-para-quem-conhece-php-5ed/",
+            "descricao": "Cras gravida. Mauris consequat aliquam leo. Aenean non tortor id metus aliquet consectetuer. Quisque sodales lectus ac orci. Donec eleifend fringilla mi. Vivamus vel massa. Aenean interdum pellentesque sem. Nulla pellentesque felis et tortor. Duis cursus, dui non dictum tincidunt, wisi ipsum mollis wisi, nec ornare velit ipsum eget enim. In sed felis. Phasellus condimentum sodales nulla. Etiam orci leo, rutrum malesuada, congue vel, fringilla vitae, lorem. Pellentesque ligula.",
+            "titulo": "PHP para quem conhece PHP - 5ª Edição",
+            "autor": "Juliano Niederauer",
+        },
+
+        {
+            "imgCapa": "https://s3.novatec.com.br/capas/9788575225462.jpg",
+            "pgLivro": "livros/linguagem-de-programacao-go/",
+            "descricao": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi eleifend, purus quis laoreet faucibus, ante augue malesuada mi, id rhoncus augue lorem eget elit. Ut sollicitudin sodales purus. Phasellus libero felis, blandit nec, commodo ut, imperdiet ut, nibh. Suspendisse potenti. Donec ullamcorper cursus dolor. Duis vitae ipsum. Maecenas dapibus hendrerit diam. Morbi varius, massa id pretium accumsan, nunc lorem congue libero, ut euismod metus libero id nulla. Duis posuere odio sed velit vulputate venenatis. Suspendisse et dui ac metus auctor fringilla. Curabitur interdum augue a pede.Curabitur hendrerit erat ut augue. Cras gravida. Mauris consequat aliquam leo. Aenean non tortor id metus aliquet consectetuer. Quisque sodales lectus ac orci. Donec eleifend fringilla mi. Vivamus vel massa. Aenean interdum pellentesque sem. Nulla pellentesque felis et tortor. Duis cursus, dui non dictum tincidunt, wisi ipsum mollis wisi, nec ornare velit ipsum eget enim. In sed felis. Phasellus condimentum sodales nulla. Etiam orci leo, rutrum malesuada, congue vel, fringilla vitae, lorem. Pellentesque ligula.",
+            "titulo": "A Linguagem de Programação Go",
+            "autor": "Alan A. Donovan & Brian W. Kernighan",
+        }
+    ]
+}
